062f1b9834b9b218607af3e09d68b3163713a8e3
Satyam <satyamd823@gmail.com>
2026-05-29 18:03:25 +0530

    Added install app button

---
+++ staticfiles/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "bookvault-v1";
+
+const urlsToCache = [
+    "/",
+    "/news/",
+    "/explore-books/"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
